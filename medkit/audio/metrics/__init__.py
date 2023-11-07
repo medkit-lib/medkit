36e3dcfe6fa3d5def298b484497499262d17d3d9
Olivier Birot <olivier.birot@inria.fr>
2023-11-07 15:52:49 +0100

    [audio] Introduce DiarizationEvaluator

---
+++ medkit/audio/metrics/__init__.py
@@ -0,0 +1,8 @@
+__all__ = []
+
+from medkit.core.utils import modules_are_available
+
+if modules_are_available(["pyannote"]) and modules_are_available(
+    ["pyannote.core", "pyannote.metrics"]
+):
+    __all__.append("diarization")
