1a0bcd2b1a7d8e13e97af24150857bd8badf8235
Olivier Birot <olivier.birot@inria.fr>
2023-11-14 15:28:30 +0100

    [metrics] Add missing import of text classification module in __init__.py

--- medkit/text/metrics/__init__.py
+++ medkit/text/metrics/__init__.py
@@ -1,5 +1,9 @@
 __all__ = []
+
 from medkit.core.utils import modules_are_available
 
 if modules_are_available(["seqeval", "transformers", "torch"]):
     __all__.append("ner")
+
+if modules_are_available(["sklearn"]):
+    __all__.append("classification")
