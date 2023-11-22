38ff800b95f7209a1fcec5c232f66c5e03194e78
Olivier Birot <olivier.birot@inria.fr>
2023-11-22 11:55:34 +0100

    [EDSNLPDateMatcher] Introduce intermediate internal method
    This reduces the indent and simplifies the diff of an upcoming commit.

--- medkit/text/ner/edsnlp_date_matcher.py
+++ medkit/text/ner/edsnlp_date_matcher.py
@@ -92,37 +92,36 @@ class EDSNLPDateMatcher(NEROperation):
 
     def _find_dates_in_segment(self, segment, spacy_doc) -> Iterator[Entity]:
         for spacy_span in spacy_doc.spans["dates"]:
-            # convert span span start/end to medkit spans relative to doc
-            text, spans = span_utils.extract(
-                segment.text,
-                segment.spans,
-                [(spacy_span.start_char, spacy_span.end_char)],
+            yield self._build_entity(segment, spacy_span)
+
+    def _build_entity(self, segment, spacy_span) -> Entity:
+        # convert span span start/end to medkit spans relative to doc
+        text, spans = span_utils.extract(
+            segment.text,
+            segment.spans,
+            [(spacy_span.start_char, spacy_span.end_char)],
+        )
+        # create attribute storing normalized date values
+        attr = build_date_attribute(spacy_span=spacy_span, spacy_label="date")
+        # create entity
+        entity = Entity(label=self.output_label, spans=spans, text=text, attrs=[attr])
+
+        # handle provenance
+        if self._prov_tracer is not None:
+            self._prov_tracer.add_prov(
+                entity, self.description, source_data_items=[segment]
             )
-            # create attribute storing normalized date values
-            attr = build_date_attribute(spacy_span=spacy_span, spacy_label="date")
-            # create entity
-            entity = Entity(
-                label=self.output_label, spans=spans, text=text, attrs=[attr]
+            self._prov_tracer.add_prov(
+                attr, self.description, source_data_items=[segment]
             )
 
-            # handle provenance
-            if self._prov_tracer is not None:
-                self._prov_tracer.add_prov(
-                    entity, self.description, source_data_items=[segment]
-                )
-                self._prov_tracer.add_prov(
-                    attr, self.description, source_data_items=[segment]
-                )
+        # copy attrs from source segment to date entity
+        for label in self.attrs_to_copy:
+            for attr in segment.attrs.get(label=label):
+                copied_attr = attr.copy()
+                entity.attrs.add(copied_attr)
+                # handle provenance
+                if self._prov_tracer is not None:
+                    self._prov_tracer.add_prov(copied_attr, self.description, [attr])
 
-            # copy attrs from source segment to date entity
-            for label in self.attrs_to_copy:
-                for attr in segment.attrs.get(label=label):
-                    copied_attr = attr.copy()
-                    entity.attrs.add(copied_attr)
-                    # handle provenance
-                    if self._prov_tracer is not None:
-                        self._prov_tracer.add_prov(
-                            copied_attr, self.description, [attr]
-                        )
-
-            yield entity
+        return entity
