37f4a456cda71c881a072f2b5d9d587dda0181b0
ARIAS Camila <deycy-camila.arias-villamil@inria.fr>
2023-11-27 11:13:06 +0000

    Resolve "Create minidocs from an annotated corpus"

---
+++ medkit/text/postprocessing/document_splitter.py
@@ -0,0 +1,277 @@
+__all__ = ["DocumentSplitter"]
+# functions to create minidocs from segments
+from typing import List, Optional
+from medkit.core import Attribute, Operation
+from medkit.core.text import (
+    Entity,
+    ModifiedSpan,
+    Relation,
+    Segment,
+    Span,
+    TextDocument,
+    TextAnnotation,
+    span_utils,
+)
+from medkit.text.postprocessing.alignment_utils import compute_nested_segments
+
+
+class DocumentSplitter(Operation):
+    """Split text documents using its segments as a reference.
+
+    The resulting 'mini-documents' contain the entities belonging to each
+    segment along with their attributes.
+
+    This operation can be used to create datasets from medkit text documents.
+    """
+
+    def __init__(
+        self,
+        segment_label: str,
+        entity_labels: Optional[List[str]] = None,
+        attr_labels: Optional[List[str]] = None,
+        relation_labels: Optional[List[str]] = None,
+        name: Optional[str] = None,
+        uid: Optional[str] = None,
+    ):
+        """
+        Instantiate the document splitter
+
+        Parameters
+        ----------
+        segment_label:
+            Label of the segments to use as references for the splitter
+        entity_labels:
+            Labels of entities to be included in the mini documents.
+            If None, all entities from the document will be included.
+        attr_labels:
+            Labels of the attributes to be included into the new annotations.
+            If None, all attributes will be included.
+        relation_labels:
+            Labels of relations to be included in the mini documents.
+            If None, all relations will be included.
+        name:
+            Name describing the splitter (default to the class name).
+        uid: str, Optional
+            Identifier of the operation
+        """
+        # Pass all arguments to super (remove self)
+        init_args = locals()
+        init_args.pop("self")
+        super().__init__(**init_args)
+
+        self.segment_label = segment_label
+        self.entity_labels = entity_labels
+        self.attr_labels = attr_labels
+        self.relation_labels = relation_labels
+
+    def run(self, docs: List[TextDocument]) -> List[TextDocument]:
+        """Split docs into mini documents
+
+        Parameters
+        ----------
+        documents:
+            List of text documents to split
+
+        Returns
+        -------
+            List of documents created from the selected segments
+        """
+        segment_docs = []
+
+        for doc in docs:
+            segments = doc.anns.get_segments(label=self.segment_label)
+
+            # filter entities
+            entities = (
+                doc.anns.get_entities()
+                if self.entity_labels is None
+                else [
+                    ent
+                    for label in self.entity_labels
+                    for ent in doc.anns.get_entities(label=label)
+                ]
+            )
+
+            # align segment and entities (fully contained)
+            segment_and_entities = compute_nested_segments(segments, entities)
+
+            # filter relations in the document
+            relations = (
+                doc.anns.get_relations()
+                if self.relation_labels is None
+                else [
+                    rel
+                    for label in self.relation_labels
+                    for rel in doc.anns.get_relations(label=label)
+                ]
+            )
+
+            # Iterate over all segments and corresponding nested entities
+            for segment, nested_entities in segment_and_entities:
+                # filter relations in nested entities
+                entities_uid = set(ent.uid for ent in nested_entities)
+                nested_relations = [
+                    relation
+                    for relation in relations
+                    if relation.source_id in entities_uid
+                    and relation.target_id in entities_uid
+                ]
+                # create new document from segment
+                segment_doc = self._create_segment_doc(
+                    segment=segment,
+                    entities=nested_entities,
+                    relations=nested_relations,
+                    doc_source=doc,
+                )
+                segment_docs.append(segment_doc)
+
+        return segment_docs
+
+    def _create_segment_doc(
+        self,
+        segment: Segment,
+        entities: List[Entity],
+        relations: List[Relation],
+        doc_source: TextDocument,
+    ) -> TextDocument:
+        """Create a TextDocument from a segment and its entities.
+        The original zone of the segment becomes the text of the document.
+
+        Parameters
+        ----------
+        segment:
+            Segment to use as reference for the new document
+        entities:
+            Entities inside the segment
+        relations:
+            Relations inside the segment
+        doc_source:
+            Initial document from which annotations where extracted
+
+        Returns
+        -------
+        TextDocument
+            A new document with entities, the metadata includes the original span and metadata
+        """
+
+        normalized_spans = span_utils.normalize_spans(segment.spans)
+
+        # create an empty mini-doc with the raw text of the segment
+        offset, end_span = normalized_spans[0].start, normalized_spans[-1].end
+        metadata = doc_source.metadata.copy()
+        metadata.update(segment.metadata)
+
+        segment_doc = TextDocument(
+            text=doc_source.text[offset:end_span], metadata=metadata
+        )
+
+        # handle provenance
+        if self._prov_tracer is not None:
+            self._prov_tracer.add_prov(
+                segment_doc, self.description, source_data_items=[segment]
+            )
+
+        # Copy segment attributes
+        segment_attrs = self._filter_attrs_from_ann(segment)
+        for attr in segment_attrs:
+            new_doc_attr = attr.copy()
+            segment_doc.attrs.add(new_doc_attr)
+            # handle provenance
+            if self._prov_tracer is not None:
+                self._prov_tracer.add_prov(
+                    new_doc_attr,
+                    self.description,
+                    source_data_items=[attr],
+                )
+
+        # Add selected entities
+        uid_mapping = {}
+        for ent in entities:
+            spans = []
+            for span in ent.spans:
+                # relocate entity spans using segment offset
+                if isinstance(span, Span):
+                    spans.append(Span(span.start - offset, span.end - offset))
+                else:
+                    replaced_spans = [
+                        Span(sp.start - offset, sp.end - offset)
+                        for sp in span.replaced_spans
+                    ]
+                    spans.append(
+                        ModifiedSpan(length=span.length, replaced_spans=replaced_spans)
+                    )
+            # define the new entity
+            relocated_ent = Entity(
+                text=ent.text,
+                label=ent.label,
+                spans=spans,
+                metadata=ent.metadata.copy(),
+            )
+            # add mapping for relations
+            uid_mapping[ent.uid] = relocated_ent.uid
+
+            # handle provenance
+            if self._prov_tracer is not None:
+                self._prov_tracer.add_prov(
+                    relocated_ent, self.description, source_data_items=[ent]
+                )
+
+            # Copy entity attributes
+            entity_attrs = self._filter_attrs_from_ann(ent)
+            for attr in entity_attrs:
+                new_ent_attr = attr.copy()
+                relocated_ent.attrs.add(new_ent_attr)
+                # handle provenance
+                if self._prov_tracer is not None:
+                    self._prov_tracer.add_prov(
+                        new_ent_attr,
+                        self.description,
+                        source_data_items=[attr],
+                    )
+
+            # add entity to the new document
+            segment_doc.anns.add(relocated_ent)
+
+        for rel in relations:
+            relation = Relation(
+                label=rel.label,
+                source_id=uid_mapping[rel.source_id],
+                target_id=uid_mapping[rel.target_id],
+                metadata=rel.metadata.copy(),
+            )
+            # handle provenance
+            if self._prov_tracer is not None:
+                self._prov_tracer.add_prov(
+                    relation, self.description, source_data_items=[rel]
+                )
+
+            # Copy relation attributes
+            relation_attrs = self._filter_attrs_from_ann(rel)
+            for attr in relation_attrs:
+                new_rel_attr = attr.copy()
+                relation.attrs.add(new_rel_attr)
+                # handle provenance
+                if self._prov_tracer is not None:
+                    self._prov_tracer.add_prov(
+                        new_rel_attr,
+                        self.description,
+                        source_data_items=[attr],
+                    )
+
+            # add relation to the new document
+            segment_doc.anns.add(relation)
+
+        return segment_doc
+
+    def _filter_attrs_from_ann(self, ann: TextAnnotation) -> List[Attribute]:
+        """Filter attributes from an annotation using 'attr_labels'"""
+        attrs = (
+            ann.attrs.get()
+            if self.attr_labels is None
+            else [
+                attr
+                for label in self.attr_labels
+                for attr in ann.attrs.get(label=label)
+            ]
+        )
+        return attrs
---
+++ tests/unit/text/postprocessing/test_document_splitter.py
@@ -0,0 +1,217 @@
+import pytest
+
+from medkit.core import Attribute, ProvTracer
+from medkit.core.text import Entity, ModifiedSpan, Segment, Span, TextDocument, Relation
+from medkit.text.postprocessing.document_splitter import DocumentSplitter
+
+
+@pytest.fixture()
+def doc():
+    text = "The medkit library. This is a    large      entity"
+
+    # a normal segment
+    segment_1 = Segment(
+        label="normal_sentence",
+        spans=[Span(0, 18)],
+        text="The medkit library",
+        attrs=[Attribute(label="segment_attr", value=0)],
+        metadata={"sent_id": "001"},
+    )
+    # modified segment
+    segment_2 = Segment(
+        label="modified_sentence",
+        spans=[
+            Span(start=20, end=29),
+            ModifiedSpan(length=1, replaced_spans=[Span(start=29, end=33)]),
+            Span(start=33, end=38),
+            ModifiedSpan(length=1, replaced_spans=[Span(start=38, end=44)]),
+            Span(start=44, end=50),
+        ],
+        text="This is a large entity",
+        metadata={"sent_id": "002"},
+    )
+    entity_1 = Entity(
+        uid="e1",
+        label="ORG",
+        text="medkit",
+        spans=[Span(4, 10)],
+        attrs=[Attribute(label="entity_attr", value=0)],
+    )
+
+    entity_2 = Entity(
+        uid="e2",
+        label="ENTITY",
+        spans=[
+            Span(start=33, end=38),
+            ModifiedSpan(length=1, replaced_spans=[Span(start=38, end=44)]),
+            Span(start=44, end=50),
+        ],
+        text="large entity",
+    )
+
+    entity_3 = Entity(
+        uid="e3",
+        label="MISC",
+        spans=[Span(20, 24)],
+        text="This",
+    )
+    # relations
+    relation_1 = Relation(label="not_related", source_id="e1", target_id="e2")
+    relation_2 = Relation(label="related", source_id="e2", target_id="e3")
+
+    anns = [segment_1, segment_2, entity_1, entity_2, entity_3, relation_1, relation_2]
+    doc = TextDocument(text=text, anns=anns, metadata={"doc_id": "001"})
+    return doc
+
+
+def test_document_splitter_no_attrs(doc):
+    splitter = DocumentSplitter(
+        segment_label="normal_sentence",
+        entity_labels=["ORG"],
+        attr_labels=[],
+        relation_labels=[],
+    )
+    new_docs = splitter.run([doc])
+    assert len(new_docs) == 1
+
+    new_doc = new_docs[0]
+    assert isinstance(new_doc, TextDocument)
+
+    assert new_doc.text == "The medkit library"
+    assert new_doc.metadata == {"sent_id": "001", "doc_id": "001"}
+    entities_doc = new_doc.anns.get_entities()
+    assert len(entities_doc) == 1
+    assert entities_doc[0].spans == [Span(4, 10)]
+
+
+def test_document_splitter_attrs(doc):
+    splitter = DocumentSplitter(
+        segment_label="normal_sentence",
+        entity_labels=["ORG"],
+        attr_labels=None,
+        relation_labels=[],
+    )
+    new_docs = splitter.run([doc])
+    assert len(new_docs) == 1
+
+    new_doc = new_docs[0]
+    assert new_doc.attrs.get(label="segment_attr")
+    assert len(new_doc.attrs.get(label="entity_attr")) == 0
+
+    entity_0 = new_doc.anns.get(label="ORG")[0]
+    assert entity_0.attrs.get(label="entity_attr")
+    assert len(entity_0.attrs.get(label="segment_attr")) == 0
+
+
+def test_with_modified_spans(doc):
+    splitter = DocumentSplitter(
+        segment_label="modified_sentence",
+        entity_labels=["ENTITY"],
+        attr_labels=[],
+        relation_labels=[],
+    )
+    new_docs = splitter.run([doc])
+    assert len(new_docs) == 1
+
+    new_doc = new_docs[0]
+    assert isinstance(new_doc, TextDocument)
+
+    assert new_doc.text == "This is a    large      entity"
+    assert new_doc.metadata == {"sent_id": "002", "doc_id": "001"}
+
+    entities_doc = new_doc.anns.get_entities()
+    assert len(entities_doc) == 1
+
+    # spans should be offset
+    entity_1 = entities_doc[0]
+    assert entity_1.spans == [
+        Span(start=13, end=18),
+        ModifiedSpan(length=1, replaced_spans=[Span(start=18, end=24)]),
+        Span(start=24, end=30),
+    ]
+    assert entity_1.text == "large entity"
+
+
+def test_with_relations(doc):
+    splitter = DocumentSplitter(
+        segment_label="modified_sentence",
+        entity_labels=["ENTITY", "MISC"],
+        attr_labels=[],
+        relation_labels=["related"],
+    )
+    new_docs = splitter.run([doc])
+    assert len(new_docs) == 1
+
+    new_doc = new_docs[0]
+    relations = new_doc.anns.get_relations()
+    assert len(relations) == 1
+
+    relation = relations[0]
+    entity_1 = new_doc.anns.get(label="ENTITY")[0]
+    entity_2 = new_doc.anns.get(label="MISC")[0]
+    assert relation.source_id == entity_1.uid
+    assert relation.target_id == entity_2.uid
+
+
+def test_prov(doc):
+    splitter = DocumentSplitter(
+        segment_label="normal_sentence",
+        entity_labels=None,  # include all entities
+        attr_labels=None,
+        relation_labels=[],
+    )
+    prov_tracer = ProvTracer()
+    splitter.set_prov_tracer(prov_tracer)
+    new_docs = splitter.run([doc])
+    new_doc = new_docs[0]
+
+    sentence_1 = doc.anns.get(label="normal_sentence")[0]
+    prov_1 = prov_tracer.get_prov(new_doc.uid)
+    assert prov_1.data_item == new_doc
+    assert prov_1.op_desc == splitter.description
+    assert prov_1.source_data_items == [sentence_1]
+
+    # check prov doc attr
+    segment_attr = sentence_1.attrs.get(label="segment_attr")[0]
+    doc_attr = new_doc.attrs.get(label="segment_attr")[0]
+    prov_2 = prov_tracer.get_prov(doc_attr.uid)
+    assert prov_2.data_item == doc_attr
+    assert prov_2.op_desc == splitter.description
+    assert prov_2.source_data_items == [segment_attr]
+
+    entity_1 = doc.anns.get(label="ORG")[0]
+    entity_1_new_doc = new_doc.anns.get(label="ORG")[0]
+    prov_3 = prov_tracer.get_prov(entity_1_new_doc.uid)
+    assert prov_3.data_item == entity_1_new_doc
+    assert prov_3.op_desc == splitter.description
+    assert prov_3.source_data_items == [entity_1]
+
+    # check prov entity attr
+    entity_attr = entity_1.attrs.get(label="entity_attr")[0]
+    new_entity_attr = entity_1_new_doc.attrs.get(label="entity_attr")[0]
+    prov_4 = prov_tracer.get_prov(new_entity_attr.uid)
+    assert prov_4.data_item == new_entity_attr
+    assert prov_4.op_desc == splitter.description
+    assert prov_4.source_data_items == [entity_attr]
+
+
+def test_prov_with_relations(doc):
+    splitter = DocumentSplitter(
+        segment_label="modified_sentence",
+        entity_labels=None,  # include all entities
+        attr_labels=["segment_attr"],
+        relation_labels=None,
+    )
+
+    prov_tracer = ProvTracer()
+    splitter.set_prov_tracer(prov_tracer)
+    new_docs = splitter.run([doc])
+    new_doc = new_docs[0]
+
+    # check provenance in the new relation
+    relation = doc.anns.get(label="related")[0]
+    new_relation = new_doc.anns.get(label="related")[0]
+    prov_1 = prov_tracer.get_prov(new_relation.uid)
+    assert prov_1.data_item == new_relation
+    assert prov_1.op_desc == splitter.description
+    assert prov_1.source_data_items == [relation]
