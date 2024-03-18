94a9f40a01aeaeac0469055ec72fd90503f5206e
Ghislain Vaillant <ghislain.vaillant@inria.fr>
2024-03-18 15:24:09 +0100

    ENH: Backport or use itertools.batched from Python 3.12

---
+++ medkit/_compat.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import sys
+from typing import Any, Iterable
+
+__all__ = ["batched"]
+
+
+def _batched(iterable: Iterable[Any], n: int) -> tuple[Any, ...]:
+    """Batch data from an iterable into n-sized tuples.
+
+    The last tuple may be shorter than the requested batch size.
+
+    Examples
+    --------
+    >>> list(_batched("ABCDEFG", 3))
+    [('A', 'B', 'C'), ('D', 'E', 'F'), ('G',)]
+    """
+    from itertools import islice
+
+    if n < 1:
+        msg = "batch size must be at least one"
+        raise ValueError(msg)
+
+    it = iter(iterable)
+    while batch := tuple(islice(it, n)):
+        yield batch
+
+
+if sys.version_info >= (3, 12):
+    from itertools import batched
+else:
+    batched = _batched
