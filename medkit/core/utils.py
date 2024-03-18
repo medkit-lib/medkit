1ca7c290fe96a7e376c36e5f3665111e5a76daa3
Ghislain Vaillant <ghislain.vaillant@inria.fr>
2024-03-18 15:56:19 +0100

    STY: Better type signature for modules_are_available

--- medkit/core/utils.py
+++ medkit/core/utils.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+from typing import Iterable
+
 __all__ = ["modules_are_available"]
 
 
-def modules_are_available(modules: list[str]):
+def modules_are_available(modules: Iterable[str]) -> bool:
     from importlib.util import find_spec
 
     return all(find_spec(m) is not None for m in modules)
