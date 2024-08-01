4bf2e044d3636a891fd50d55dc9ec8c2de1b69db
Clara Petrescu-Stompor <clara.petrescu-stompor@inria.fr>
2024-08-01 14:55:42 +0200

    ajout de la classe FrDateRecognizer et FrAgeRecognizer

--- medkit/text/deid.py
+++ medkit/text/deid.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Iterator
+from typing import TYPE_CHECKING, Iterator, List, Optional
 
 from medkit._import import import_optional
 from medkit.core.text import Entity, span_utils
@@ -11,7 +11,10 @@ if TYPE_CHECKING:
 
 presidio_analyzer = import_optional("presidio_analyzer", extra="deid")
 
-__all__ = ["PIIDetector"]
+from presidio_analyzer import Pattern, PatternRecognizer
+
+__all__ = ["PIIDetector", "FrDateRecognizer","FrAgeRecognizer"]
+
 
 
 class PIIDetector(NEROperation):
@@ -35,3 +38,87 @@ class PIIDetector(NEROperation):
                 spans=spans,
                 metadata={"score": result.score},
             )
+
+    
+class FrDateRecognizer(PatternRecognizer):
+    """
+    Recognizes French Date using regex.
+
+    :param patterns: List of patterns to be used by this recognizer
+    :param supported_language: Language this recognizer supports
+    :param supported_entity: The entity this recognizer can detect
+    """
+    PATTERNS = [
+        Pattern(
+            "French dates with day month year",
+            r"\b(?i)(\d{1,2}|1er) ((janvier|janv.|JAN)|(février|févr.|FÉV)|(mars|MAR)|(avril|avr.|AVR)|(mai|MAI)|(juin|JUN)|(juillet|juill.|JUL)|(août|AOÛ)|(septembre|sept.|SEP)|(octobre|oct.|OCT)|(novembre|nov.|NOV)|(décembre|déc|DÉC)) \d{4}\b",
+            0.9
+        ),
+        Pattern(
+            "French dates with month year",
+            r"\b(?i)((janvier|janv.|JAN)|(février|févr.|FÉV)|(mars|MAR)|(avril|avr.|AVR)|(mai|MAI)|(juin|JUN)|(juillet|juill.|JUL)|(août|AOÛ)|(septembre|sept.|SEP)|(octobre|oct.|OCT)|(novembre|nov.|NOV)|(décembre|déc|DÉC)) \d{4}\b",
+            0.9
+        ),
+        Pattern(
+            "French dates with day month",
+            r"\b(?i)(\d{1,2} |1er )((janvier|janv.|JAN)|(février|févr.|FÉV)|(mars|MAR)|(avril|avr.|AVR)|(mai|MAI)|(juin|JUN)|(juillet|juill.|JUL)|(août|AOÛ)|(septembre|sept.|SEP)|(octobre|oct.|OCT)|(novembre|nov.|NOV)|(décembre|déc|DÉC))\b",
+            0.9
+        ),
+        Pattern(
+            "French dates with month",
+            r"\b(?i)((janvier|janv.|JAN)|(février|févr.|FÉV)|(mars|MAR)|(avril|avr.|AVR)|(mai|MAI)|(juin|JUN)|(juillet|juill.|JUL)|(août|AOÛ)|(septembre|sept.|SEP)|(octobre|oct.|OCT)|(novembre|nov.|NOV)|(décembre|déc|DÉC))\b",
+            0.9
+        ),
+    ]
+
+    def __init__(
+        self,
+        patterns: Optional[List[Pattern]] = None,
+        supported_language: str = "fr",
+        supported_entity : str = "FR_DATE",
+    ):
+        patterns = patterns if patterns else self.PATTERNS
+        super().__init__(
+            supported_entity=supported_entity,
+            patterns=patterns,
+            supported_language=supported_language,
+        )
+
+class FrAgeRecognizer(PatternRecognizer):
+    """
+    Recognizes French Phone Age using regex.
+
+    :param patterns: List of patterns to be used by this recognizer
+    :param supported_language: Language this recognizer supports
+    :param supported_entity: The entity this recognizer can detect
+    """
+    PATTERNS = [
+        Pattern(
+            "French age written out numbers .. (ans|mois)' ou ' .. et .. ans' ou ' .. à .. ans' ou ' .. ou .. ans'",
+             r"\d+\s+(ans|mois)|\d+\s+et\s+\d+\s+(ans|mois)|\d+\s+à\s+\d+\s+(ans|mois)|\d+\s+ou\s+\d+\s+(ans|mois)",
+            0.9
+        ),
+        Pattern(
+            "French age written out numbers 'ans et .. mois' ou 'ans et .. ou .. mois' ou 'ans et .. à .. mois' ",
+            r"\d+\s+ans\s+et\s+\d+\s+mois|\d+\s+ans\s+et\s+\d+\s+à\s+\d+\s+mois|\d+\s+ans\s+et\s+\d+\s+ou\s+\d+\s+mois",
+            0.9
+        ),
+        Pattern(
+            "French age written out with letters '.. (ans|mois)' ou ' .. et .. ans' ou ' .. à .. ans' ou ' .. ou .. ans' ",
+            r"((dix|vingt|trente|quarante|cinquante|soixante|soixante-dix|quatre-vingt|quatre-vingt-dix|cent)(\s+|-|-et-)?)?(un|deux|trois|quatre|cinq|six|sept|huit|neuf|dix|onze|douze|treize|quatorze|quinze|seize)?\s+((et\s+|à\s+|ou\s+)\d+\s+)?(ans|mois)",
+            0.9
+        ),
+    ]
+
+    def __init__(
+        self,
+        patterns: Optional[List[Pattern]] = None,
+        supported_language: str = "fr",
+        supported_entity : str = "FR_AGE",
+    ):
+        patterns = patterns if patterns else self.PATTERNS
+        super().__init__(
+            supported_entity=supported_entity,
+            patterns=patterns,
+            supported_language=supported_language,
+        )
